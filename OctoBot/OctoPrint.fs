af7dcd36bca723cb029f9a73845e74c897968a98
lf- <lf-@users.noreply.github.com>
2019-08-04 01:21:25 -0600

    asyncify this shit. Some of it could be rendered less readable using more functional constructs

--- OctoBot/OctoPrint.fs
+++ OctoBot/OctoPrint.fs
@@ -2,7 +2,10 @@
 open System.Text
 open FSharp.Data
 open Newtonsoft.Json
+open Util
 
+// XXX: mutable global. And the functions will throw exceptions if it's not set.
+// this is an anti-functional variable
 let mutable octoUri = ""
 
 let makeOctoUri sub =
@@ -11,7 +14,7 @@ let makeOctoUri sub =
     builder.Uri.AbsoluteUri
 
 let _octoRequest method data path =
-    Http.Request (
+    Http.AsyncRequest (
         url = (makeOctoUri path),
         ?body = data,
         headers = [ ("Accept", "application/json"); ("Content-Type", HttpContentTypes.Json) ],
@@ -22,8 +25,10 @@ let octoGet = _octoRequest HttpMethod.Get None
 let octoPost req = _octoRequest HttpMethod.Post (Some req)
 
 let getWorkflowSupported () =
-    let res = octoGet "/plugin/appkeys/probe"
-    (int res.StatusCode) = 204
+    async {
+        let! res = octoGet "/plugin/appkeys/probe"
+        return (int res.StatusCode) = 204
+    }
 
 type KeyRequest = { app: string; user: string }
 
@@ -35,11 +40,13 @@ type KeyRequestResp = JsonProvider<"""{ "app_token": "abcdef" }""">
 /// <returns>AppToken to use in pollDecision</returns>
 /// <seealso cref="pollDecision" />
 let keyRequest appName userId =
-    let req = JsonConvert.SerializeObject {app=appName; user=userId}
-    let resp = octoPost (TextRequest req) "/plugin/appkeys/request"
-    match resp.Body with
-    | Text(t) -> Some (KeyRequestResp.Parse t).AppToken
-    | Binary(_) -> None
+    async {
+        let req = JsonConvert.SerializeObject {app=appName; user=userId}
+        let! resp = octoPost (TextRequest req) "/plugin/appkeys/request"
+        match resp.Body with
+        | Text(t) -> return Some (KeyRequestResp.Parse t).AppToken
+        | Binary(_) -> return None
+    }
 
 type AppKeyGrantedResp = JsonProvider<"""{ "api_key": "apikey" }""">
 
@@ -51,27 +58,37 @@ type AppKeyDecision =
 /// <summary>Check if authorization has completed yet. Call every <5 seconds.</summary>
 /// <param name="appToken">AppToken from <see cref="keyRequest">keyRequest</see></param>
 let pollDecision appToken =
-    let res = octoGet (sprintf "/plugin/appkeys/request/%s" appToken)
-    match int res.StatusCode with
-    | 200 ->
-        match res.Body with
-        | Text(t) ->
-            let appkey = AppKeyGrantedResp.Parse t
-            Granted(appkey.ApiKey)
-        | Binary(_) -> Failed
-    | 202 -> PollAgain
-    | _ -> Failed
+    async {
+        let! res = octoGet (sprintf "/plugin/appkeys/request/%s" appToken)
+        match int res.StatusCode with
+        | 200 ->
+            match res.Body with
+            | Text(t) ->
+                let appkey = AppKeyGrantedResp.Parse t
+                return Granted(appkey.ApiKey)
+            | Binary(_) -> return Failed
+        | 202 -> return PollAgain
+        | _ -> return Failed
+    }
 
 
-let rec waitForDecision appToken pollInterval =
-    let pollInt = defaultArg pollInterval 2
-    match pollDecision appToken with
-    | PollAgain ->
-        System.Threading.Thread.Sleep pollInt
-        waitForDecision appToken pollInterval
-    | Granted(s) -> Some s
-    | Failed -> None
+let rec waitForDecision appToken =
+    async {
+        let! decision = pollDecision appToken
+        match decision with
+        | PollAgain ->
+                do! (Async.Sleep 2)
+                return! (waitForDecision appToken)
+        | Granted(s) -> return Some s
+        | Failed -> return None
+    }
 
 
-let testReqProcess () =
-    keyRequest "app" "lf" |> Option.map waitForDecision
+let requestApiKey appName userId =
+    // XXX: there isn't anything wrong with this code but we could make it less readable
+    //      using functional constructs
+    async {
+        match! keyRequest appName userId with
+        | None -> return None
+        | Some tok -> return! waitForDecision tok
+    }
